ecd3cf5dd264c2620fe8010bc3f7cf8938279094
wujinfeng <jim.wu2580@gmail.com>
2025-09-15 18:47:07 -0400

    new

--- src/DataTypes.java
+++ src/DataTypes.java
@@ -1,6 +1,6 @@
 public class DataTypes {
-    // TODO TASK 1: fix this code so that it passes the test in DataTypesTest.java
     public static long sum(int[] numbers) {
+        if (numbers == null) return 0;
         int s = 0; // variable to accumulate the sum in!
         // below is a "foreach" loop which iterates through numbers
         for (int x : numbers) {
